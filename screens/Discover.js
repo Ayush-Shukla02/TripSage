2e8bc814e6be34ecb1809870f0fc83015c303f28
sleepinGiant <shukla.ayush0203@gmail.com>
2023-06-20 01:56:24 +0530

    Discover Page Menu, Search Bar

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,10 +1,22 @@
-import { View, Text, SafeAreaView, StatusBar, Image } from "react-native";
-import React, { useLayoutEffect } from "react";
+import {
+	View,
+	Text,
+	SafeAreaView,
+	StatusBar,
+	Image,
+	ScrollView,
+} from "react-native";
+import React, { useLayoutEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { Avatar } from "../assets";
+import { Attractions, Avatar, Hotels, Restaurants } from "../assets";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import Config from "react-native-config";
+import MenuContainer from "../components/MenuContainer";
 
 const Discover = () => {
 	const navigation = useNavigation();
+
+	const [type, setType] = useState("restaurants");
 
 	useLayoutEffect(() => {
 		navigation.setOptions({
@@ -14,7 +26,7 @@ const Discover = () => {
 
 	return (
 		<SafeAreaView
-			style={{ marginTop: StatusBar.currentHeight }}
+			style={{ marginTop: StatusBar.currentHeight + 10 }}
 			className="flex-1 relative"
 		>
 			<View className="flex-row items-center justify-between px-8">
@@ -33,6 +45,50 @@ const Discover = () => {
 					/>
 				</View>
 			</View>
+
+			<View className="flex-row items-center bg-white mx-4 rounded-xl py-1 px-4 shadow-lg mt-4">
+				<GooglePlacesAutocomplete
+					GooglePlacesDetailsQuery={{ fields: "geometry" }}
+					placeholder="Search"
+					fetchDetails={true}
+					onPress={(data, details = null) => {
+						// 'details' is provided when fetchDetails = true
+						console.log(details?.geometry?.viewport);
+					}}
+					query={{
+						key: "AIzaSyAEDnXkSs0nntwSGUFm5Jz4jQvdKGBWfo8",
+						language: "en",
+					}}
+					onFail={(error) => console.error(error)}
+				/>
+			</View>
+
+			<ScrollView>
+				<View className="flex-row items-center justify-between px-8 mt-8">
+					<MenuContainer
+						key={"attractions"}
+						title="Attractions"
+						imageSrc={Attractions}
+						type={type}
+						setType={setType}
+					/>
+					<MenuContainer
+						key={"hotel"}
+						title="Hotels"
+						imageSrc={Hotels}
+						type={type}
+						setType={setType}
+					/>
+
+					<MenuContainer
+						key={"restaurants"}
+						title="Restaurants"
+						imageSrc={Restaurants}
+						type={type}
+						setType={setType}
+					/>
+				</View>
+			</ScrollView>
 		</SafeAreaView>
 	);
 };
